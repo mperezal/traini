0f5e1ab4051682b571e230ed35e5c287325286d7
mperezal <87032627+mperezal@users.noreply.github.com>
2024-06-06 21:35:31 +0200

    Update index.js

--- .github/actions/ftp-deploy-action/index.js
+++ .github/actions/ftp-deploy-action/index.js
@@ -1,1 +1,19 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    const server = core.getInput('server');
+    const username = core.getInput('username');
+    const password = core.getInput('password');
+    const localDir = core.getInput('local-dir');
+    const serverDir = core.getInput('server-dir');
+
+    await exec.exec(`lftp -c "open -u ${username},${password} ${server}; mirror -R ${localDir} ${serverDir}"`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
 
